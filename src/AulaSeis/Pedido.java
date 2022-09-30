957f15d7be1375398aa3bb82aab61fdc92904d64
Renan <renanhenrikecorrea@hotmail.com>
2022-09-30 21:36:15 +0100

    Mackenzie

--- src/AulaSeis/Pedido.java
+++ src/AulaSeis/Pedido.java
@@ -1,19 +1,27 @@
 package AulaSeis;
 
-public class Pedido {
+public class Pedido  {
     int numero;
     int quantidade;
-    double valor;
+    double valor = 0;
     Produto item;
 
-    Pedido (int numero, int quantidade, Produto item){
+    Pedido (int numero, Produto item, int quantidade){
         this.numero = numero;
         this.quantidade = quantidade;
-        item = new Produto ();
+        this.item = item;
     }
 
-    public void calculaPedido (double preco){
-        valor = preco * quantidade;
+    public String toString(){
+        calculaPedido();
+
+        return  "Número do pedido:" + numero + "\nCódigo do Produto: " + item.cod +
+                "\nDescrição: " + item.descricao +
+                "\nPreço do Produto: "+ item.preco + "\nQuantidade: "+ quantidade+
+                "\nValor total do pedido: "+ valor;
     }
 
+    public void calculaPedido() {
+        valor = quantidade * item.preco;
+    }
 }
--- src/AulaSeis/Produto.java
+++ src/AulaSeis/Produto.java
@@ -1,4 +1,13 @@
 package AulaSeis;
 
 public class Produto {
+    int cod;
+    String descricao;
+    double preco;
+
+    Produto (int cod, String descricao,double preco){
+        this.cod = cod;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
 }
